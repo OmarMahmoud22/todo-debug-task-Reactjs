d969258e20d513038e28495b4add6b44827506fe
OmarMahmou22 <omahmoudali9@gmail.com>
2026-05-12 18:55:30 +0300

    fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 const Home = () => {
-  const userName = "Ezz";
+  const user_Name = "Ezz";
 
   return (
     <div className="container py-5">
       <div className="bg-white p-5 rounded-4 shadow-sm border-0">
         <h1 className="display-4 fw-bold text-dark">
-          Welcome Back, {user_name}! 👋
+          Welcome Back, {user_Name}! 👋
         </h1>
         <p className="lead text-muted">You have 3 tasks to complete today.</p>
         <hr className="my-4" />
