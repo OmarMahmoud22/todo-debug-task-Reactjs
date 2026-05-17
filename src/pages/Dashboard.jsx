97f952738693839791e7851988afc78c8b1453d3
OmarMahmou22 <omahmoudali9@gmail.com>
2026-05-17 12:33:39 +0300

    adding_data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,14 @@
 import Footer from "../components/Dashborad/Footer/Footer";
 import Navbar from "../components/Dashborad/Navbar/Navbar";
-
+import Sidebar from "../components/Dashborad/Sidebar/Sidebar";
 function Dashboard() {
   return (
     <>
       <Navbar adminName="mohamed" />
-      <h1>Test Dashboard</h1>
+      <main className="d-flex">
+      <Sidebar />
+      <h1 className="flex-grow-1">Test Dashboard</h1>
+      </main>
       <Footer />
     </>
   );
